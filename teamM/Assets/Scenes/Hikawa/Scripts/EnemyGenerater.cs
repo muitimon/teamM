1a9211a93951e29754077ee0a5df47d0f76e505e
k-hikawa <ev40548@meiji.ac.jp>
2018-09-08 18:15:21 +0900

    一定時間ごとにEnemyを生成するスクリプトを作成

---
+++ teamM/Assets/Scenes/Hikawa/Scripts/EnemyGenerater.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerater : MonoBehaviour {
+
+	private int count = 0;
+	public int speed = 60;
+
+	public GameObject EnemyPrefs;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+		if(count >= speed){
+			//オブジェクトの座標
+			float x = Random.Range(-10.0f, 10.0f);
+			float y = Random.Range(0.0f, 0.0f);
+			float z = Random.Range(0.0f, 0.0f);
+			GameObject Enemy = Instantiate(EnemyPrefs, new Vector3(x, y, z), Quaternion.identity);
+			Enemy.transform.parent = transform;
+			count = 0;
+		}
+		count++;
+	}
+}
