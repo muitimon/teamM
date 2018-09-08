e689539d3dea7edcb02052c7523121588e7e7828
k-hikawa <ev40548@meiji.ac.jp>
2018-09-08 17:57:50 +0900

    EnemyがIdolを追跡するスクリプトを作成

---
+++ teamM/Assets/Scenes/Hikawa/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	private GameObject Idol;
+	private float rotateSpeed = 2f;
+
+	[SerializeField] public float speed = 1f;
+
+	void Start () {
+		Idol = GameObject.FindGameObjectWithTag("Idol");
+	}
+	
+	void Update () {
+		// Idolの方向を向く
+		Transform target = Idol.transform;
+		this.transform.LookAt(target);
+		// 前進する
+		transform.Translate(Vector3.forward * Time.deltaTime * speed * 1 );
+	}
+}
