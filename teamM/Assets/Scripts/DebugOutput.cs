602db1e11398f33eb54b30a250561c7ad6923e34
internship05 <internship05@internship05-no-MacBook-puro.local>
2018-09-09 08:10:40 +0900

    player first commit

---
+++ teamM/Assets/Scripts/DebugOutput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DebugOutput : MonoBehaviour {
+
+	private static DebugOutput inst;
+
+	public static void Write(string str)
+	{
+		inst._Write(str);
+	}
+
+	void Start()
+	{
+		inst = this;
+	}
+
+	void _Write(string str)
+	{
+		StartCoroutine(Upload(str));
+	}
+
+	IEnumerator Upload(string data) {
+		var loggingServerURL = "http://b5ae5c08.ngrok.io";
+		var www = UnityWebRequest.Post(loggingServerURL, data);
+		yield return www.SendWebRequest();
+	}
+}
